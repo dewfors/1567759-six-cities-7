4075f8c426b50b2427296e6ffc46acf92e7fee6c
Zaur Malakhov <1zaur1@mail.ru>
2021-06-09 12:31:22 +0300

    3-1-1 Добавит моки

---
+++ project/src/mocks/offers.js
@@ -0,0 +1,262 @@
+const offersJSON = `[
+  {
+    "city": {
+      "name": "Hamburg",
+      "location": {
+        "latitude": 53.550341,
+        "longitude": 10.000654,
+        "zoom": 13
+      }
+    },
+    "preview_image": "https://7.react.pages.academy/static/hotel/15.jpg",
+    "images": [
+      "https://7.react.pages.academy/static/hotel/15.jpg",
+      "https://7.react.pages.academy/static/hotel/1.jpg",
+      "https://7.react.pages.academy/static/hotel/20.jpg",
+      "https://7.react.pages.academy/static/hotel/8.jpg",
+      "https://7.react.pages.academy/static/hotel/9.jpg",
+      "https://7.react.pages.academy/static/hotel/14.jpg",
+      "https://7.react.pages.academy/static/hotel/13.jpg",
+      "https://7.react.pages.academy/static/hotel/7.jpg",
+      "https://7.react.pages.academy/static/hotel/16.jpg",
+      "https://7.react.pages.academy/static/hotel/10.jpg",
+      "https://7.react.pages.academy/static/hotel/12.jpg",
+      "https://7.react.pages.academy/static/hotel/4.jpg",
+      "https://7.react.pages.academy/static/hotel/19.jpg",
+      "https://7.react.pages.academy/static/hotel/18.jpg"
+    ],
+    "title": "Beautiful & luxurious apartment at great location",
+    "is_favorite": false,
+    "is_premium": false,
+    "rating": 4.5,
+    "type": "room",
+    "bedrooms": 1,
+    "max_adults": 3,
+    "price": 279,
+    "goods": [
+      "Baby seat",
+      "Laptop friendly workspace",
+      "Washer",
+      "Towels",
+      "Breakfast",
+      "Air conditioning"
+    ],
+    "host": {
+      "id": 25,
+      "name": "Angelina",
+      "is_pro": true,
+      "avatar_url": "img/avatar-angelina.jpg"
+    },
+    "description": "Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.",
+    "location": {
+      "latitude": 53.550341,
+      "longitude": 9.980654000000001,
+      "zoom": 16
+    },
+    "id": 1
+  },
+  {
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "preview_image": "https://7.react.pages.academy/static/hotel/20.jpg",
+    "images": [
+      "https://7.react.pages.academy/static/hotel/17.jpg",
+      "https://7.react.pages.academy/static/hotel/3.jpg",
+      "https://7.react.pages.academy/static/hotel/5.jpg",
+      "https://7.react.pages.academy/static/hotel/19.jpg",
+      "https://7.react.pages.academy/static/hotel/10.jpg",
+      "https://7.react.pages.academy/static/hotel/1.jpg",
+      "https://7.react.pages.academy/static/hotel/6.jpg",
+      "https://7.react.pages.academy/static/hotel/14.jpg",
+      "https://7.react.pages.academy/static/hotel/4.jpg",
+      "https://7.react.pages.academy/static/hotel/9.jpg",
+      "https://7.react.pages.academy/static/hotel/2.jpg",
+      "https://7.react.pages.academy/static/hotel/16.jpg",
+      "https://7.react.pages.academy/static/hotel/18.jpg",
+      "https://7.react.pages.academy/static/hotel/11.jpg"
+    ],
+    "title": "Canal View Prinsengracht",
+    "is_favorite": false,
+    "is_premium": true,
+    "rating": 2.6,
+    "type": "hotel",
+    "bedrooms": 3,
+    "max_adults": 6,
+    "price": 282,
+    "goods": [
+      "Laptop friendly workspace"
+    ],
+    "host": {
+      "id": 25,
+      "name": "Angelina",
+      "is_pro": true,
+      "avatar_url": "img/avatar-angelina.jpg"
+    },
+    "description": "I rent out a very sunny and bright apartment only 7 minutes walking distance to the metro station. The apartment has a spacious living room with a kitchen, one bedroom and a bathroom with mit bath. A terrace can be used in summer.",
+    "location": {
+      "latitude": 48.834610000000005,
+      "longitude": 2.335499,
+      "zoom": 16
+    },
+    "id": 2
+  },
+  {
+    "city": {
+      "name": "Amsterdam",
+      "location": {
+        "latitude": 52.37454,
+        "longitude": 4.897976,
+        "zoom": 13
+      }
+    },
+    "preview_image": "https://7.react.pages.academy/static/hotel/19.jpg",
+    "images": [
+      "https://7.react.pages.academy/static/hotel/20.jpg",
+      "https://7.react.pages.academy/static/hotel/16.jpg",
+      "https://7.react.pages.academy/static/hotel/3.jpg",
+      "https://7.react.pages.academy/static/hotel/11.jpg",
+      "https://7.react.pages.academy/static/hotel/14.jpg",
+      "https://7.react.pages.academy/static/hotel/12.jpg",
+      "https://7.react.pages.academy/static/hotel/18.jpg",
+      "https://7.react.pages.academy/static/hotel/1.jpg",
+      "https://7.react.pages.academy/static/hotel/2.jpg",
+      "https://7.react.pages.academy/static/hotel/4.jpg",
+      "https://7.react.pages.academy/static/hotel/8.jpg",
+      "https://7.react.pages.academy/static/hotel/17.jpg",
+      "https://7.react.pages.academy/static/hotel/10.jpg",
+      "https://7.react.pages.academy/static/hotel/13.jpg"
+    ],
+    "title": "Penthouse, 4-5 rooms + 5 balconies",
+    "is_favorite": false,
+    "is_premium": false,
+    "rating": 2.9,
+    "type": "room",
+    "bedrooms": 1,
+    "max_adults": 2,
+    "price": 101,
+    "goods": [
+      "Coffee machine",
+      "Towels",
+      "Cable TV",
+      "Dishwasher",
+      "Laptop friendly workspace",
+      "Fridge",
+      "Breakfast",
+      "Air conditioning",
+      "Washing machine",
+      "Washer",
+      "Baby seat"
+    ],
+    "host": {
+      "id": 25,
+      "name": "Angelina",
+      "is_pro": true,
+      "avatar_url": "img/avatar-angelina.jpg"
+    },
+    "description": "This is a place for dreamers to reset, reflect, and create. Designed with a 'slow' pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.",
+    "location": {
+      "latitude": 52.385540000000006,
+      "longitude": 4.902976,
+      "zoom": 16
+    },
+    "id": 3
+  },
+  {
+    "city": {
+      "name": "Cologne",
+      "location": {
+        "latitude": 50.938361,
+        "longitude": 6.959974,
+        "zoom": 13
+      }
+    },
+    "preview_image": "https://7.react.pages.academy/static/hotel/12.jpg",
+    "images": [
+      "https://7.react.pages.academy/static/hotel/17.jpg",
+      "https://7.react.pages.academy/static/hotel/1.jpg",
+      "https://7.react.pages.academy/static/hotel/6.jpg",
+      "https://7.react.pages.academy/static/hotel/2.jpg",
+      "https://7.react.pages.academy/static/hotel/10.jpg",
+      "https://7.react.pages.academy/static/hotel/20.jpg",
+      "https://7.react.pages.academy/static/hotel/9.jpg",
+      "https://7.react.pages.academy/static/hotel/18.jpg",
+      "https://7.react.pages.academy/static/hotel/8.jpg",
+      "https://7.react.pages.academy/static/hotel/14.jpg",
+      "https://7.react.pages.academy/static/hotel/7.jpg",
+      "https://7.react.pages.academy/static/hotel/3.jpg",
+      "https://7.react.pages.academy/static/hotel/19.jpg",
+      "https://7.react.pages.academy/static/hotel/5.jpg"
+    ],
+    "title": "The Joshua Tree House",
+    "is_favorite": false,
+    "is_premium": false,
+    "rating": 2.8,
+    "type": "room",
+    "bedrooms": 1,
+    "max_adults": 2,
+    "price": 201,
+    "goods": [
+      "Coffee machine",
+      "Dishwasher",
+      "Breakfast",
+      "Washer",
+      "Laptop friendly workspace",
+      "Towels",
+      "Baby seat",
+      "Air conditioning",
+      "Fridge"
+    ],
+    "host": {
+      "id": 25,
+      "name": "Angelina",
+      "is_pro": true,
+      "avatar_url": "img/avatar-angelina.jpg"
+    },
+    "description": "Discover daily local life in city center, friendly neighborhood, clandestine casino, karaoke, old-style artisans, art gallery and artist studio downstairs.",
+    "location": {
+      "latitude": 50.934361,
+      "longitude": 6.943974,
+      "zoom": 16
+    },
+    "id": 4
+  }
+]`;
+
+const getAdaptedToClientOffer = (offer) => {
+  return {
+    city: offer.city,
+    previewImage: offer.preview_image,
+    images: offer.images,
+    title: offer.title,
+    isFavorite: offer.is_favorite,
+    isPremium: offer.is_premium,
+    rating: offer.rating,
+    type: offer.type,
+    bedrooms: offer.bedrooms,
+    maxAdults: offer.max_adults,
+    price: offer.price,
+    goods: offer.goods,
+    host: {
+      id: offer.id,
+      name: offer.name,
+      isPro: offer.is_pro,
+      avatarUrl: offer.avatar_url,
+    },
+    description: offer.description,
+    location: offer.location,
+    id: offer.id,
+  }
+}
+
+const getAdaptedToClientOffers = () => {
+  const offers = JSON.parse(offersJSON);
+  return offers.map((offer) => getAdaptedToClientOffer(offer));
+}
+
+export {getAdaptedToClientOffers};
