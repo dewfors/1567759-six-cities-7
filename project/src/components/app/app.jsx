59c313d1648fa1dfec1dd8a966f80a186ce546fd
Zaur Malakhov <1zaur1@mail.ru>
2021-06-09 13:18:55 +0300

    3-1-2 Подключит моки и добавит валидацию

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -7,10 +7,12 @@ import PageFavorites from '../page-favorites/page-favorites';
 import PageNotFound from '../page-not-found/page-not-found';
 import PageOffer from '../page-offer/page-offer';
 import PageLogin from '../page-login/page-login';
+import PropTypes from "prop-types";
 
 const placesToStay = 515;
 
-function App() {
+function App(props) {
+  const {offers} = props;
   return (
     <BrowserRouter>
       <Switch>
@@ -35,5 +37,44 @@ function App() {
 
   );
 }
+
+App.propTypes = {
+  offers: PropTypes.arrayOf(
+    PropTypes.shape({
+      city: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        location: PropTypes.shape({
+          latitude: PropTypes.number.isRequired,
+          longitude: PropTypes.number.isRequired,
+          zoom: PropTypes.number.isRequired,
+        }).isRequired,
+      }).isRequired,
+      previewImage: PropTypes.string.isRequired,
+      images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+      title: PropTypes.string.isRequired,
+      isFavorite: PropTypes.bool.isRequired,
+      isPremium: PropTypes.bool.isRequired,
+      rating: PropTypes.number.isRequired,
+      type: PropTypes.string.isRequired,
+      bedrooms: PropTypes.number.isRequired,
+      maxAdults: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      goods: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+      host: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        isPro: PropTypes.bool.isRequired,
+        avatarUrl: PropTypes.string.isRequired,
+      }).isRequired,
+      description: PropTypes.string.isRequired,
+      location: PropTypes.shape({
+        latitude: PropTypes.number.isRequired,
+        longitude: PropTypes.number.isRequired,
+        zoom: PropTypes.number.isRequired,
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {getAdaptedToClientOffers} from './mocks/offers.js'
+
+const offers = getAdaptedToClientOffers();
+console.log(offers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offers={offers}/>
   </React.StrictMode>,
   document.getElementById('root'));
