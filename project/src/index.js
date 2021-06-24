81bbcf35fea9e8a95bf3b52eb7fc47e707a261ba
Zaur Malakhov <1zaur1@mail.ru>
2021-06-24 10:57:47 +0300

    5-1-3 Инициализтрует хранилище

--- project/src/index.js
+++ project/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {getAdaptedToClientComments, getAdaptedToClientOffers} from './utils/utils';
 import {CITY} from './mocks/city';
+import {reduser} from './store/reduser';
 
 const offers = getAdaptedToClientOffers();
 const comments = getAdaptedToClientComments();
 
+const store = createStore(reduser);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App offers={offers} comments={comments} city={CITY}/>
+    <Provider store={store}>
+      <App offers={offers} comments={comments} city={CITY}/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
