e466694402aed823b134da8b3d730454f20039c5
Zaur Malakhov <1zaur1@mail.ru>
2021-07-20 14:08:42 +0300

    6-2-1 Разобьет редьюсер в соответствии с предметной областью

---
+++ project/src/store/redusers/reduser-reviews/reviews.js
@@ -0,0 +1,64 @@
+import {ActionType} from '../../action';
+import {Settings} from '../../../utils/const';
+
+const SORT_TYPE_DEFAULT = 'Popular';
+
+const initialState = {
+  reviews: [],
+  reviewsStatus: {
+    isLoading: true,
+    isSuccess: false,
+    isError: false,
+  },
+
+  sendNewReviewStatus: {
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+  },
+
+};
+
+const reviews = (state = initialState, action) => {
+
+  switch (action.type) {
+    case ActionType.LOAD_REVIEWS_REQUEST:
+      return {
+        ...state,
+        reviewsStatus: { ...state.reviewsStatus, isLoading: true },
+      };
+    case ActionType.LOAD_REVIEWS_SUCCESS:
+      return {
+        ...state,
+        reviewsStatus: { ...state.reviewsStatus, isLoading: true },
+        reviews: action.payload,
+      };
+    case ActionType.LOAD_REVIEWS_ERROR:
+      return {
+        ...state,
+        reviewsStatus: { ...state.reviewsStatus, isLoading: false, isError: true },
+      };
+
+
+    case ActionType.SEND_NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        sendNewReviewStatus: { ...state.sendNewReviewStatus, isLoading: true },
+      };
+    case ActionType.SEND_NEW_REVIEW_SUCCESS:
+      return {
+        ...state,
+        sendNewReviewStatus: { ...state.sendNewReviewStatus, isLoading: false, isSuccess: true },
+        reviews: action.payload,
+      };
+    case ActionType.SEND_NEW_REVIEW_ERROR:
+      return {
+        ...state,
+        sendNewReviewStatus: { ...state.sendNewReviewStatus, isLoading: false, isError: true },
+      };
+    default:
+      return state;
+  }
+};
+
+export {reviews};
---
+++ project/src/store/redusers/reduser-user/user.js
@@ -0,0 +1,79 @@
+import {ActionType} from '../../action';
+import {AuthorizationStatus, Settings} from '../../../utils/const';
+
+const SORT_TYPE_DEFAULT = 'Popular';
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.UNKNOWN,
+  userInfo: {},
+  loginStatus: {
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+  },
+  logoutStatus: {
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+  },
+};
+
+const user = (state = initialState, action) => {
+
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return {
+        ...state,
+        authorizationStatus: action.payload,
+      };
+
+
+    case ActionType.SET_USER_INFO:
+      return {
+        ...state,
+        userInfo: action.payload,
+      };
+    case ActionType.LOGIN_REQUEST:
+      return {
+        ...state,
+        loginStatus: { ...state.loginStatus, isLoading: true },
+      };
+    case ActionType.LOGIN_SUCCESS:
+      return {
+        ...state,
+        loginStatus: { ...state.loginStatus, isLoading: false, isSuccess: true },
+      };
+    case ActionType.LOGIN_ERROR:
+      return {
+        ...state,
+        loginStatus: { ...state.loginStatus, isLoading: false, isError: true },
+      };
+
+    case ActionType.LOGOUT_REQUEST:
+      return {
+        ...state,
+        logoutStatus: { ...state.loginStatus, isLoading: true },
+      };
+    case ActionType.LOGOUT_SUCCESS:
+      return {
+        ...state,
+        logoutStatus: { ...state.loginStatus, isLoading: false, isSuccess: true },
+      };
+    case ActionType.LOGOUT_ERROR:
+      return {
+        ...state,
+        logoutStatus: { ...state.loginStatus, isLoading: false, isError: true },
+      };
+
+
+    case ActionType.LOGOUT:
+      return {
+        ...state,
+        authorizationStatus: AuthorizationStatus.NO_AUTH,
+      };
+    default:
+      return state;
+  }
+};
+
+export {user};
