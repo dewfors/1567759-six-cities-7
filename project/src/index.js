a193fc6553f356d451807599c2b40e039d833a73
Zaur Malakhov <1zaur1@mail.ru>
2021-07-21 12:51:44 +0300

    6-3-4 Обновит код создания хранилища

--- project/src/index.js
+++ project/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import {createAPI} from './services/api';
 import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+// import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {getAdaptedToClientComments} from './utils/utils';
 import {CITY} from './mocks/city';
-import {reduser} from './store/reduser';
-import rootReduser from './store/redusers/root-ruduser';
+// import {reduser} from './store/reduser';
+import rootReducer from './store/redusers/root-ruduser';
 import {AuthorizationStatus, Settings} from './utils/const';
 import {ActionCreator} from './store/action';
 import {checkAuth, fetchHotels} from './store/api-actions';
@@ -21,14 +22,25 @@ const api = createAPI(() => {
 
 const comments = getAdaptedToClientComments();
 
-const store = createStore(
-  // reduser,
-  rootReduser,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(Redirect),
-  ),
-);
+// const store = createStore(
+//   // reduser,
+//   rootReduser,
+//   composeWithDevTools(
+//     applyMiddleware(thunk.withExtraArgument(api)),
+//     applyMiddleware(Redirect),
+//   ),
+// );
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }).concat(Redirect),
+});
+
+
 
 store.dispatch(checkAuth());
 store.dispatch(fetchHotels());
